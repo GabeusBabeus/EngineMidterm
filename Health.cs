f1b1a08612db7088fc52ad5a652afd0f4273da33
GabeusBabeus <gabriel.mercier@ontariotechu.net>
2022-10-05 14:19:00 -0400

    Game and Script

---
+++ Health.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    //public static GameManager Instance;
+    public int PlayerHealth;
+    
+    void Awake()
+    {
+        //if (//Instance == null)
+      //  {
+           // Instance = this;
+        //    DontDestroyOnLoad(this);
+          //  PlayerHealth = 100f;
+        //}
+        //else 
+
+        //{
+          //  Destroy(this);
+        //}
+    }
+
+    //public void SceneRestart()
+    //{
+      //  if (PlayerHealth <= 0f)
+        //{
+          //  SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //}
+    //}
+} 
+
